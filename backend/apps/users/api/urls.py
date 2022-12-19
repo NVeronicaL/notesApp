30c1c2045ae81407c00b4d09247fd27c5b53cfcc
nveronical <verito.cba933@gmail.com>
2022-12-19 18:24:08 -0300

    Agregando la url de login

--- backend/apps/users/api/urls.py
+++ backend/apps/users/api/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 from apps.users.api.views import (
     UserApiView,
     CreateUserApiView, 
-    UserDetailApiView
+    UserDetailApiView,
+    AuthLoginAPIView
 )
 
 # Urls
@@ -13,5 +14,6 @@ urlpatterns = [
     path('user-list/', UserApiView.as_view(), name='user_list'),
     path('create-user/', CreateUserApiView.as_view(), name='create'),
     path('detail-user/<int:pk>/', UserDetailApiView.as_view(), name='details'),
+    path('login/', AuthLoginAPIView.as_view(), name='auth'),
 ]
 
