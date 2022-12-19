8a2b1df47aa8b72df0309106297f807078772c7a
nveronical <verito.cba933@gmail.com>
2022-12-18 22:33:00 -0300

    Implementación de API para nota

---
+++ frontend/src/components/nota/NotaServer.js
@@ -0,0 +1,33 @@
+// Nos conectamos a la API
+const API_URL = "http://127.0.0.1:8000/nota/";
+
+export const getNota = async (id) => {
+    return await fetch(API_URL+'detail-nota/'+id);
+}
+
+export const updateNota = async(id) => {
+    return await fetch(API_URL+'detail-nota/'+id);
+}
+
+export const listNota = async() => {
+    return await fetch(API_URL+'nota-list',{
+        method: "GET"
+    });
+}
+
+export const deleteNota = async(id) => {
+    return await fetch(API_URL+'detail-nota/'+id);
+}
+
+export const createNota = async(newNota) => {
+    console.log('newNota:', newNota)
+
+    return await fetch(API_URL+'create-nota/',{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newNota),
+        // mode: 'no-cors',
+    });
+}
