f619fda8ed2dc567efb1da21ad87a9378102d6bd
nveronical <verito.cba933@gmail.com>
2022-12-18 22:34:42 -0300

    Implementación de Navbar

---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Menu = () => {
+
+    return(
+     <div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+            <Navbar.Brand href="#home">App of Notes</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                <Nav.Link href="/register">Registro</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/notas">Notas</Nav.Link>
+                <Nav.Link href="/create-nota">Nota</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+     </div>       
+  );
+}
+
+export default Menu;
