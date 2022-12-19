7285962a8ae5e743c159b3928250fd64b16d1ead
nveronical <verito.cba933@gmail.com>
2022-12-18 22:30:58 -0300

    Creación de archivo FormLogin

---
+++ frontend/src/components/user/FormLogin.js
@@ -0,0 +1,15 @@
+// import { render } from "@testing-library/react";
+import React, {useState, useEffect} from "react";
+// import {useParams, useNavigate} from "reac-router-dom";
+
+import * as Server from './UserServer';
+
+const FormLogin = () => {
+
+    return(
+        <p>Falta...</p>
+    )
+}
+
+export default FormLogin;
+
